312b63941b29513f716ccb6eb123b6f6d27050fb
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-22 13:32:48 +0900

    unfinish

---
+++ beg_041/D3.hs
@@ -0,0 +1,51 @@
+--from http://abc041.contest.atcoder.jp/assignments Question D
+-- Time Over in this algorithm
+import Data.List
+
+main = do
+  p <- getContents
+  let n = read $ words (lines p !! 0) !! 0 ::Int
+      m = read $ words (lines p !! 0) !! 1 ::Int
+      set = map ((map read).words) $ tail $ lines p ::[[Int]]
+  putStr $ show $ getResult' n set 
+getResultForPrint :: [Maybe [Int]] -> [[Int]]
+getResultForPrint (Nothing :xs)  = getResultForPrint xs
+getResultForPrint (Just s : xs)  = s : getResultForPrint xs
+getResultForPrint [] = []
+
+getResult' :: Int -> [[Int]] -> Int                       
+getResult' n xss = sum $ map ( judge_int xss ) $ permutations [1..n] 
+judge_int :: [[Int]] -> [Int] -> Int
+judge_int xss xs | judge' xss xs = 1 --judge or judge'
+                 | otherwise = 0
+-- n -> set -> result             
+getResult :: Int -> [[Int]] -> [Maybe [Int]]
+getResult n xss = map (judge_just xss) $ permutations [1..n]  
+
+judge_just:: [[Int]] -> [Int] -> Maybe [Int]
+judge_just xss ys | judge xss ys  = Just ys
+                  | otherwise  = Nothing
+                 
+-- information -> list -> Bool
+judge :: [[Int]] -> [Int] -> Bool
+judge ( (x:[y]):xss) ys | (ind_x < ind_y) = True && (judge xss ys)
+                   | otherwise = False
+  where ind_x = elemIndex x ys
+        ind_y = elemIndex y ys
+judge [] ys = True
+
+judge' :: [[Int]] -> [Int] -> Bool
+judge' xss n = ( judge' xss (n - 1) ) && judge (filter (\(x:[y]) -> x <= n && y <= n ) xss ) (permutations n)
+judge' _ 1 = True                        
+judge' [] _ = True
+
+judge' :: [[Int]] -> n -> [[Int]]
+judge' set n = rejudge set $ map (mypush n) (judge' set (n-1)) 
+               
+mypush :: a -> [a] -> [a]
+mypush x xs = 
+
+
+
+
+
